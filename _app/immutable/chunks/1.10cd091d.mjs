de40b0ef424c9865d9368a51c22e62ecca91b23b
Izzat Razab <redizz501@gmail.com>
2023-02-23 22:33:23 +0800

    Updates

---
+++ _app/immutable/chunks/1.10cd091d.mjs
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.b2131897.mjs";export{t as component};
--- _app/immutable/chunks/1.67925e24.mjs
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.cbaa9ffb.mjs";export{t as component};
